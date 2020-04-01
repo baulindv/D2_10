b86d30da4975555a2d692493dd752401ba036394
baulindv <40152406+baulindv@users.noreply.github.com>
2020-04-01 12:50:47 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -2,8 +2,7 @@ from bottle import Bottle, request
 import sentry_sdk
 from sentry_sdk.integrations.bottle import BottleIntegration
 
-sentry_sdk.init(dsn='',
-                integrations=[BottleIntegration()])
+sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegration()])
 
 app = Bottle()
 
