bc898fbe4c2fcfc4b4de4d5ebf1d4801da4bdfef
baulindv <40152406+baulindv@users.noreply.github.com>
2020-04-01 12:21:17 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,21 @@
+from bottle import Bottle, request
+import sentry_sdk
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(dsn='',
+                integrations=[BottleIntegration()])
+
+app = Bottle()
+
+
+@app.route('/success')
+def success():
+    return
+
+
+@app.route('/fail')
+def fail():
+    raise RuntimeError("Ошибка!")
+
+
+app.run(host='localhost', port=8080)
